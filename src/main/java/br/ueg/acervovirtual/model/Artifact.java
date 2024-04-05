4d292bed9dc72c42759d10ce16ebb469cabb619f
Lucas Apparecido de Oliveira <108204218+LucasApparecido@users.noreply.github.com>
2024-04-05 09:29:52 -0300

    Atualização com a conexão do banco de dados
    (Eu so queria trocar de computador mesmo)

--- src/main/java/br/ueg/acervovirtual/model/Artifact.java
+++ src/main/java/br/ueg/acervovirtual/model/Artifact.java
@@ -28,36 +28,36 @@ public class Artifact {
     @Column(name = "Id", nullable = false)
     private Long id;
 
-    @Column(name = "Numero_da_peca", length = 20)
+    @Column(name = "Numero_da_peca", length = 20, nullable = false)
     private String pieceNumber;
 
-    @Column(name = "Proveniencia", length = 100)
+    @Column(name = "Proveniencia", length = 100, nullable = false)
     private String provenance;
 
-    @Column(name = "Coletor_Doador", length = 100)
+    @Column(name = "Coletor_Doador", length = 100, nullable = false)
     private String collectorDonor;
 
-    @Column(name = "Familia_Taxon", length = 250)
+    @Column(name = "Familia_Taxon", length = 250, nullable = false)
     private String familyTaxon;
 
-    @Column(name = "Ano_de_Coleta", length = 4)
+    @Column(name = "Ano_de_Coleta", length = 4, nullable = false)
     private Integer collectionYear;
 
-    @Column(name = "Localizacao_no_Acervo", length = 20)
+    @Column(name = "Localizacao_no_Acervo", length = 20, nullable = false)
     private String locationInCollection;
 
-    @Column(name = "Periodo_Epoca_Idade", length = 50)
+    @Column(name = "Periodo_Epoca_Idade", length = 50, nullable = false)
     private String periodEpochAge;
 
-    @Column(name = "Colecao", length = 30)
+    @Column(name = "Colecao", length = 30, nullable = false)
     private String collection;
 
     @Column(name = "Descricao_da_Peca", length = 500)
     private String pieceDescription;
 
-    @Column(name = "Status")
+    @Column(name = "Status", nullable = false)
     private Boolean status;
 
-    @Column(name = "Data_de_tombamento")
+    @Column(name = "Data_de_tombamento", nullable = false)
     private LocalDate tombingDate;
 }
